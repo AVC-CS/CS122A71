4ef4d2a110e764055b586932cc07d780c1e4c3fa
Kyu Lee <kwooler@gmail.com>
2025-10-19 16:22:08 -0700

    A71 init

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ def getFarNumber(numbers):
     Code Your Program here
     ########################################
     """
-
+    avg = sum(numbers) / len(numbers)
+    deviations = [abs(x - avg) for x in numbers]
+    max_deviation = max(deviations)
+    max_index = deviations.index(max_deviation)
+    return numbers[max_index]
 
 def main():
     numbers = [10, 5, 20, 0, 40, 45, 50, 55, 9, 10]
-    # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20]
+    print(f'\n################ Test 1 ########################')
+    print(f'Original numbers: {numbers}')
     ret = getFarNumber(numbers)
-    print(f'Your return value is {ret}')
+    print(f'Your return value is {ret}, expected value is 55')
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20]
+    print(f'\n################ Test 2 ########################')
+    print(f'Original numbers: {numbers}')
+    ret = getFarNumber(numbers)
+    print(f'Your return value is {ret}, expected value is 20')
 
 if __name__ == '__main__':
     main()
--- main_test.py
+++ main_test.py
@@ -1,55 +1,31 @@
 import main
-import io
-import sys
-import re
 import math
+import pytest
 
-
+@pytest.mark.basic
 def test_main_1():
-    # captureOut = io.StringIO()
-    # sys.stdout = captureOut
-    # # datastr = '10 5 20 0 40 45 50 55 9 10\n'
-    # # sys.stdin = io.StringIO(datastr)
-
-    # sys.stdout = sys.__stdout__
-    # print('Captured ', captureOut.getvalue())
-    # lines = captureOut.getvalue().split('\n')
-    # print(lines)
-
     numbers = [10, 5, 20, 0, 40, 45, 50, 55, 9, 10]
     ret = main.getFarNumber(numbers)
     print(f'Your return value is {ret}')
-    assert ret == 55
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
+    assert ret == 55, f'Expected 55 but got {ret}'
 
-
+@pytest.mark.edge
 def test_main_2():
-    # captureOut = io.StringIO()
-    # sys.stdout = captureOut
-    # # datastr = '1 2 3 4 5 6 7 8 9 10\n'
-    # # sys.stdin = io.StringIO(datastr)
-
-    # sys.stdout = sys.__stdout__
-    # print('Captured ', captureOut.getvalue())
-    # lines = captureOut.getvalue().split('\n')
-    # print(lines)
-
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20]
     ret = main.getFarNumber(numbers)
     print(f'Your return value is {ret}')
-    assert ret == 20
-    # regex_string = r'[\w,\W]*1'
-    # regex_string += r'[\w,\W]*3'
-    # regex_string += r'[\w,\W]*5'
-    # regex_string += r'[\w,\W]*'
-    # print(regex_string)
-    # res = re.search(regex_string, main.evenlist)
-    # assert res != None
-    # print(res.group())
+    assert ret == 20, f'Expected 20 but got {ret}'
+
+@pytest.mark.bonus
+def test_main_3():
+    numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 500]
+    ret = main.getFarNumber(numbers)
+    print(f'Your return value is {ret}')
+    assert ret == 500, f'Expected 500 but got {ret}'
+
+@pytest.mark.bonus
+def test_main_4():
+    numbers = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
+    ret = main.getFarNumber(numbers)
+    print(f'Your return value is {ret}')
+    assert ret == 10, f'Expected 10 but got {ret}'
